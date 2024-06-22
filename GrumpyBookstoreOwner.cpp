28a198d7fd06ce318ad5f1b272772f4a39a57082
Striver-3110 <21csjay067@ldce.ac.in>
2024-06-22 18:00:46 +0530

    (grumpy book store owner)3 different approaches

--- GrumpyBookstoreOwner.cpp
+++ GrumpyBookstoreOwner.cpp
@@ -4,60 +4,146 @@
 
 using namespace std;
 
+
 class Solution {
 public:
-    int maxSatisfied(vector<int>& NotGrumpy, vector<int>& grumpy, int minutes) {
-        vector<pair<pair<int, int>, int>> windowDiff;
-        if (NotGrumpy.size() == 1) {
-            return NotGrumpy[0];
+    int maxSatisfied(vector<int>& NotGrupmy, vector<int>& grumpy, int minutes) {
+
+        vector<pair<pair<int,int>,int>> windowDiff;
+        if(NotGrupmy.size() == 1){
+            return NotGrupmy[0];
         }
-        vector<int> Grumpy;
+        vector<int> Grupmy;
         int grumpySum = 0, NotGrumpySum = 0, totalSum = 0;
-        for (int i = 0; i < NotGrumpy.size(); i++) {
-            totalSum += NotGrumpy[i];
-            if (grumpy[i] == 0) {
-                Grumpy.push_back(NotGrumpy[i]);
-            } else {
-                Grumpy.push_back(0);
+        for(int i = 0; i < NotGrupmy.size(); i++){
+            totalSum += NotGrupmy[i];
+            if(grumpy[i] == 0){
+                Grupmy.push_back(NotGrupmy[i]);
             }
-            if (i < minutes) {
-                NotGrumpySum += NotGrumpy[i];
-                grumpySum += Grumpy[i];
+            else{
+                Grupmy.push_back(0);
+            }
+            if(i < minutes){
+                NotGrumpySum += NotGrupmy[i];
+                grumpySum += Grupmy[i];
             }
         }
 
-        windowDiff.push_back({{0, minutes - 1}, NotGrumpySum - grumpySum});
+        windowDiff.push_back({{0,minutes-1},NotGrumpySum - grumpySum});
         int i = 1, j = minutes;
 
-        int n = NotGrumpy.size();
+        int n = NotGrupmy.size();
 
-        while (j < n) {
-            NotGrumpySum = NotGrumpySum - NotGrumpy[i - 1] + NotGrumpy[j];
-            grumpySum = grumpySum - Grumpy[i - 1] + Grumpy[j];
-            windowDiff.push_back({{i, j}, NotGrumpySum - grumpySum});
+        while(j < n){
+            NotGrumpySum = NotGrumpySum - NotGrupmy[i-1] + NotGrupmy[j];
+            grumpySum = grumpySum - Grupmy[i-1]  + Grupmy[j];
+            windowDiff.push_back({{i,j},NotGrumpySum - grumpySum});
             i++;
             j++;
         }
 
-        sort(windowDiff.begin(), windowDiff.end(), [](pair<pair<int, int>, int> &a, pair<pair<int, int>, int> &b) {
+        sort(windowDiff.begin(),windowDiff.end(),[](pair<pair<int,int>,int> &a ,pair<pair<int,int>,int> &b){
             return a.second > b.second;
         });
-
-        i = windowDiff[0].first.first;
-        j = windowDiff[0].first.second;
+       
+        i = windowDiff[0].first.first; j = windowDiff[0].first.second;
         int ans = 0;
 
-        for (int k = 0; k < n; k++) {
-            if (k < i || k > j)
-                ans += Grumpy[k];
-            else {
-                ans += NotGrumpy[k];
+        for(int k = 0; k < n ; k++){
+            if(k < i || k > j)
+                ans += Grupmy[k];
+            else{
+                ans += NotGrupmy[k];
+            }
+        }
+        return ans;
+    }
+
+    int maxSatisfied(vector<int>& NotGrupmy, vector<int>& grumpy, int minutes) {
+
+        if(NotGrupmy.size() == 1){
+            return NotGrupmy[0];
+        }
+        vector<int> Grupmy;
+        int grumpySum = 0, NotGrumpySum = 0, totalSum = 0;
+        for(int i = 0; i < NotGrupmy.size(); i++){
+            totalSum += NotGrupmy[i];
+            if(grumpy[i] == 0){
+                Grupmy.push_back(NotGrupmy[i]);
+            }
+            else{
+                Grupmy.push_back(0);
+            }
+            if(i < minutes){
+                NotGrumpySum += NotGrupmy[i];
+                grumpySum += Grupmy[i];
             }
         }
 
+        int i = 1, j = minutes;
+
+        int n = NotGrupmy.size();
+
+        int maxSum =  NotGrumpySum - grumpySum;
+        pair<int,int> pr = {0,minutes-1};
+
+        while(j < n){
+            NotGrumpySum = NotGrumpySum - NotGrupmy[i-1] + NotGrupmy[j];
+            grumpySum = grumpySum - Grupmy[i-1]  + Grupmy[j];
+            if(maxSum < NotGrumpySum - grumpySum){
+                maxSum = NotGrumpySum - grumpySum;
+                pr = {i,j};
+            }
+            i++;
+            j++;
+        }
+
+        int ans = 0;
+
+        i = pr.first; 
+        j = pr.second;
+
+        for(int k = 0; k < n ; k++){
+            if(k < i || k > j)
+                ans += Grupmy[k];
+            else{
+                ans += NotGrupmy[k];
+            }
+        }
         return ans;
     }
+
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        int satisfied = 0;
+        int n = customers.size();
+        // if the owner is not grumpy then all the customer will be satisfied
+        for(int i = 0; i < n; i++){
+            if(grumpy[i] == 0){
+                satisfied += customers[i];
+                customers[i] = 0;
+            }
+        }
+        // now we will just focus on the customers thoes who were not satisfied
+        int notSatisfied = 0;
+        int i = 0;
+        while(i < minutes){
+            notSatisfied += customers[i++];
+        }
+
+        int max = notSatisfied;
+
+        while(i < n){
+            notSatisfied += customers[i];
+            notSatisfied -= customers[i-minutes];
+            max = std::max(max,notSatisfied);
+            i++;
+        }
+
+        return max + satisfied;
+
+    }
 };
+
 
 int main() {
     Solution solution;
